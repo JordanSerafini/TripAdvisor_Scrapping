f02f01cb67e8c345ef0faca0192e3e1cfa727764
JordanSerafini <jordan@solution-logique.fr>
2024-09-20 16:46:47 +0200

    working

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 import googleSearch from './googleSearch.js';
+import pkg from 'pg';
+const { Client } = pkg;
+
+
+const client = new Client({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: parseInt(process.env.DB_PORT, 10),
+});
+
+console.log(client);
+
 
 const resto = 'les moustaches de zebulon 74 annecy';
 
-googleSearch(`tripadvisor ${resto}`);
+//googleSearch(`tripadvisor ${resto}`);
